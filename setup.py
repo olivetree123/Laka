5405206e3aa007224d85f3313ad9d8b743212085
olivetree123 <olivetree123@163.com>
2019-10-15 20:28:38 +0800

    readme

--- setup.py
+++ setup.py
@@ -23,4 +23,11 @@ setup(
     install_requires = [
         "redis",
     ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
 )
